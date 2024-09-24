7e6f3608363314b9f239ce67ef59b380ab1670dd
Lucas-eadania <lupa6@student.eadania.dk>
2024-09-24 12:08:05 +0200

    Startup

---
+++ Lucas-Variabler-og-Datatyper-Opgave/Lucas-Variabler-og-Datatyper-Opgave/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucas_Variabler_og_Datatyper_Opgave
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
